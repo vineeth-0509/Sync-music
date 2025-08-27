6ebd06c42d642645d0acad6d4f88c573714ff659
Vineeth-0509 <vineeththungani@gmail.com>
2025-08-27 15:33:06 +0530

    youtubeapisearch getting response

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,8 +1,10 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import youtubesearchapi, { GetVideoDetails } from "youtube-search-api";
 
-const  YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+// const YT_REGEX =
+//   /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 export const CreateStream = z.object({
   creatorId: z.string(),
   url: z.string(),
@@ -20,23 +22,69 @@ export async function GET(req: NextRequest) {
   });
 }
 
+// export async function POST(req: NextRequest) {
+//   try {
+//     const data = CreateStream.parse(await req.json());
+//     const isYt = data.url.match(YT_REGEX);
+//     const extractedId = data.url.split("?v=")[1] : null;
+//     if (!isYt || !extractedId) {
+//       return NextResponse.json(
+//         {
+//           message: "Wrong Url format",
+//         },
+//         {
+//           status: 400,
+//         }
+//       );
+//     }
+
+//     // const res = await youtubesearchapi.GetVideoDetails(extractedId);
+//     // console.log(res);
+//     const stream = await prismaClient.stream.create({
+//       data: {
+//         userId: data.creatorId,
+//         url: data.url,
+//         extractedId,
+//         type: "Youtube",
+//       },
+//     });
+//     return NextResponse.json({
+//       message: "Added stream",
+//       id: stream.id,
+//     });
+//   } catch (e) {
+//     return NextResponse.json(
+//       {
+//         message: "Something went wrong in creating the streams",
+//       },
+//       {
+//         status: 411,
+//       }
+//     );
+//   }
+// }
+
+const YT_REGEX =
+  /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+
 export async function POST(req: NextRequest) {
   try {
     const data = CreateStream.parse(await req.json());
-    const isYt = data.url.match(YT_REGEX);
-    if (!isYt) {
+    const match = data?.url.match(YT_REGEX);
+    const extractedId = match ? match[1] : null;
+    if (!match || !extractedId) {
       return NextResponse.json(
         {
-          message: "Wrong Url format",
+          message: "Wrong url format",
         },
         {
-          status: 411,
+          status: 400,
         }
       );
     }
-
-    const extractedId = data.url.split("?v=")[1];
-   const stream =  await prismaClient.stream.create({
+    const res = await GetVideoDetails(extractedId);
+    console.log("Youtube API responses:", res);
+    const stream = await prismaClient.stream.create({
       data: {
         userId: data.creatorId,
         url: data.url,
@@ -45,13 +93,14 @@ export async function POST(req: NextRequest) {
       },
     });
     return NextResponse.json({
-      message:"Added stream",
-      id: stream.id
-    })
-  } catch (e) {
+      message: "Added stream",
+      id: stream.id,
+    });
+  } catch (error) {
+    console.error("Post stream error:", error);
     return NextResponse.json(
       {
-        message: "Something wrong in creating stream",
+        message: "Something went wrong in creating the streams",
       },
       {
         status: 411,
