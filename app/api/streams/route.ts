3b508d9c8cd7f2ecc6cdc3663c71dc3be877f5c5
Vineeth-0509 <vineeththungani@gmail.com>
2025-08-27 11:14:10 +0530

    at the position route.ts file

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,20 +2,36 @@ import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
+const YT_REGEX = new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}$")
+
 export const CreateStream = z.object({
   creatorId: z.string(),
-  url: z
-    .url()
-    .refine((val) => val.includes("youtube") || val.includes("spotify"), {
-      message: "URL must be from youtube or spotify.",
-    }),
+  url: z.string()
 });
 
 export async function POST(req: NextRequest) {
   try {
     const data = CreateStream.parse(await req.json());
+    const isYt = YT_REGEX.test(data.url);
+    if(!isYt){
+      return NextResponse.json(
+      {
+        message: "Wrong Url format",
+      },
+      {
+        status: 411,
+      }
+    );
+    }
+    const extractedId = data.split("?v=")[1];
     prismaClient.stream.create({
-        userId: data.creatorId
+      data:{
+        userId: data.creatorId,
+        url: data.url,
+        extractedId,
+        type:
+      }
+        
     })
   } catch (error) {
     return NextResponse.json(
