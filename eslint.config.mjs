85610a5f4c881fdf1f601e81621afddbf4880536
Vineeth-0509 <vineeththungani@gmail.com>
2025-08-30 13:11:07 +0530

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,9 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/no-this-alias": "off",
     },
   },
 ];
